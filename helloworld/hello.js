f4a5d164539c5fb8fd40ebe712a3a47f6797f07b
Vihan Agarwal <vihanagarwal2003@gmail.com>
2024-10-04 21:50:27 +0530

    first commit

---
+++ helloworld/hello.js
@@ -0,0 +1,18 @@
+console.log("Hey there! i am js");
+//to run this file we need to ride node hello.js or node hello
+
+//we need to use node js to run javascript outside it engine node js in a runtime environment
+// console.log(window);
+// console.log(alert("hey")); 
+
+//these 2 lines wont run here because we didnt directly integrate it with cpp , we dont have window elements like navigator and all 
+//only core functionalities are there of server side
+
+//some features are added also for example cryptographic technique and exception handling
+
+
+//to create package.json file we will use npm init this is a configuritional file we could make this manually also but it has a risk of erros
+
+
+// we can create our own scripts in this package.json like type 'npm run customscript'
+//now using this we can make different scripts like on start ,on login 
